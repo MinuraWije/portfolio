a24b0c58d7153e48a0ad71fc87547618a05cd29a
MinuraWije <minurawije@gmail.com>
2025-03-29 11:01:52 +0530

    added google docs clone to projects

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -69,6 +69,34 @@ export const Projects = () => {
 
                         </div>
 
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2">Google Docs Clone</h3>
+                            <p className="text-gray-400 mb-4">
+                                A google docs clone project completed with react and firebase.
+                                Was also done as my first hands-on experience in building with firebase.
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["React","Firebase","CSS"].map((tech,key) => (
+                                    <span
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 mx-1 rounded-full text-sm hover:bg-blue-500/20
+                                                        hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                                    ">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+
+                            <div className="flex justify-between items-center">
+                                <a
+                                    href="https://github.com/MinuraWije/docs-clone"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                                    View Project
+                                </a>
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
             </RevealOnScroll>
